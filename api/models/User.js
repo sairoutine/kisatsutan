de4c758b615954fd4ec4ca48a4c60ea0ddb0d6a7
sairoutine <sairoutine@gmail.com>
2016-06-04 00:56:21 +0900

    add: user model

---
+++ api/models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+	connection: 'mysql1',
+	attributes: {
+		id: {
+			type: 'integer',
+			size: 64
+		},
+		name: {
+			type: 'string'
+		},
+	}
+};
+
