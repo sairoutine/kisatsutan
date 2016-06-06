994ebe5bad8a69504846cb40f6676234298cbf13
sairoutine <sairoutine@gmail.com>
2016-06-07 00:56:02 +0900

    add: user quest model

---
+++ api/models/UserQuest.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// ユーザーのクエスト進捗管理テーブル
+
+module.exports = {
+	connection: 'mysql1',
+	attributes: {
+		// サロゲートキー
+		id: {
+			type: 'integer',
+			size: 64,
+			primaryKey: true,
+		},
+		user_id: { // PK1
+			type: 'integer',
+			size: 64,
+		},
+		// 章
+		chapter: { // PK2
+			type: 'integer',
+			size: 32,
+		},
+		// 節
+		paragragh: {
+			type: 'integer',
+			size: 32
+		},
+	}
+};
+
